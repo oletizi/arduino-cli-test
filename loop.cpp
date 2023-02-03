61663394df83ed72d6886c87232984aed842b20b
orion <oletizi@mac.com>
2023-02-02 21:35:38 -0800

    - Sorted out some of the include path issues. - Moved the program logic from arduino-cli-test.ino to loop.cpp

--- loop.cpp
+++ loop.cpp
@@ -2,12 +2,85 @@
 // Created by Orion Letizi on 2/2/23.
 //
 #include <Arduino.h>
+#include <SD.h>
+#include <SPI.h>
 #include "loop.h"
 
+File myFile;
+
+// change this to match your SD shield or module;
+// Arduino Ethernet shield: pin 4
+// Adafruit SD shields and modules: pin 10
+// Sparkfun SD shield: pin 8
+// Teensy audio board: pin 10
+// Teensy 3.5 & 3.6 & 4.1 on-board: BUILTIN_SDCARD
+// Wiz820+SD board: pin 4
+// Teensy 2.0: pin 0
+// Teensy++ 2.0: pin 20
+// const int chipSelect = 10;
+
+const int chipSelect = 10;
+
 void doSetup() {
-    Serial.println("in doSetup()");
+    //UNCOMMENT THESE TWO LINES FOR TEENSY AUDIO BOARD:
+    //SPI.setMOSI(7);  // Audio shield has MOSI on pin 7
+    //SPI.setSCK(14);  // Audio shield has SCK on pin 14
+
+    // Open serial communications and wait for port to open:
+    Serial.begin(9600);
+    while (!Serial) { ; // wait for serial port to connect.
+    }
+
+
+    Serial.print("Initializing SD card...");
+
+    if (!SD.begin(chipSelect)) {
+        Serial.println("initialization failed!");
+        return;
+    }
+    Serial.println("initialization done.");
+
+    // open the file.
+    myFile = SD.open("test.txt", FILE_WRITE);
+
+    // if the file opened okay, write to it:
+    if (myFile) {
+        Serial.print("Writing to test.txt...");
+        myFile.println("testing 1, 2, 3.");
+        // close the file:
+        myFile.close();
+        Serial.println("done.");
+    } else {
+        // if the file didn't open, print an error:
+        Serial.println("error opening test.txt");
+    }
+
+    // re-open the file for reading:
+    myFile = SD.open("test.txt");
+    if (myFile) {
+        Serial.println("Opened test.txt:");
+
+        // read from the file until there's nothing else in it:
+        Serial.println("text.txt contents:");
+        while (myFile.available()) {
+            Serial.write(myFile.read());
+        }
+        // close the file:
+        myFile.close();
+    } else {
+        // if the file didn't open, print an error:
+        Serial.println("error opening test.txt");
+    }
 }
 
+unsigned long now = 0;
+unsigned long checkpoint = 0;
+
 void doLoop() {
-    Serial.println("in doLoop()");
+    // nothing happens after setup
+    now = millis();
+    if (now - checkpoint >= 1000) {
+        checkpoint = now;
+        Serial.printf("At the tone, the time will be: %lu\n", now);
+    }
 }
