aae70a9cd5d55c3c989e820c9b18f76627e87924
orion <oletizi@mac.com>
2023-02-02 19:40:21 -0800

    basics seem to work.

---
+++ loop.cpp
@@ -0,0 +1,12 @@
+//
+// Created by Orion Letizi on 2/2/23.
+//
+#include <Arduino.h>
+#include "loop.h"
+void doSetup() {
+    Serial.println("in doSetup()");
+}
+
+void doLoop() {
+    Serial.println("in doLoop()");
+}
---
+++ loop.h
@@ -0,0 +1,9 @@
+//
+// Created by Orion Letizi on 2/2/23.
+//
+
+#ifndef ARDUINO_CLI_TEST_FOO_H
+#define ARDUINO_CLI_TEST_FOO_H
+void doSetup();
+void doLoop();
+#endif //ARDUINO_CLI_TEST_FOO_H
